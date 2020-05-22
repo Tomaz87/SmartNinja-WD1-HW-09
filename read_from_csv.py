fc58df9f3e668b47ce2eed3eca7a68a557e8fcda
Tomaz87 <sturm.tomaz@gmail.com>
2020-05-22 17:14:26 +0200

    Added title of the program

--- read_from_csv.py
+++ read_from_csv.py
@@ -1,5 +1,7 @@
 import csv
 
+print("Hello! This program reads users data from csv and show them in the sentences.")
+
 with open("user_list.csv") as users_introduction:
     reader = csv.DictReader(users_introduction)
 
