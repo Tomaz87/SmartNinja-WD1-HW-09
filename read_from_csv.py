66d38edf87bd46ca360041c0f8c6378d0ea68086
Tomaz87 <sturm.tomaz@gmail.com>
2020-05-25 18:33:47 +0200

    f-strings formatting implemented

--- read_from_csv.py
+++ read_from_csv.py
@@ -1,9 +1,10 @@
 import csv
 
 print("Hello! This program reads users data from csv and show them in the sentences.")
+print()
 
 with open("user_list.csv") as users_introduction:
     reader = csv.DictReader(users_introduction)
 
     for row in reader:
-        print(row["Name"] + " is " + row["Gender"] + " and is " + row["Age"] + " years old.")
+        print(f"{row['Name']} is {row['Gender']} and is {row['Age']} years old.")
