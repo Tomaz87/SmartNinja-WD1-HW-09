513040eb552fcb10aea8e04211e8c9464cc6ddfd
Tomaz87 <sturm.tomaz@gmail.com>
2020-05-25 18:31:02 +0200

    Added csv library.

--- write_to_csv.py
+++ write_to_csv.py
@@ -1,9 +1,11 @@
+import csv
+
 print("Hello! This program collects users data and store them in the csv file.")
 
 print("Fill in required data below please.")
-name = str(input("First name: ")).capitalize()
+name = input(f"First name: ").capitalize()
 age = int(input("Age: "))
-gender = str(input("Gender: ")).lower()
+gender = input(f"Gender: ").lower()
 
 with open("user_list.csv", "a") as list_of_users:
     list_of_users.write(f'{name},{age},{gender}'"\n")
